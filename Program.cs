927f85786f4e75a99d8537beb3fd6f51cbaede25
Jeff Sahol <jeff@sahol.net>
2012-10-21 17:38:59 -0400

    just the program, no config, no registering as default app

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ChromeShim
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string url = Environment.GetCommandLineArgs().ElementAtOrDefault(1);
+
+            if (!String.IsNullOrEmpty(url))
+            {
+                url = @"""" + url + @"""";
+                string chromePath = Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), @"Google\Chrome\Application\chrome.exe");
+                Process.Start(chromePath, url);
+            }
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+        }
+    }
+}
